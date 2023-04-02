729b4f0a74b789f02f0c698e72751403b186427d
Richard <zohassadar@gmail.com>
2023-04-01 17:16:13 -0700

    Update with separate starting characters

--- gfx/nametables/generate.sh
+++ gfx/nametables/generate.sh
@@ -3,43 +3,43 @@ set -e
 
 python nametable_builder.py break \
     enter_high_score_nametable.bak \
-    characters.txt \
+    characters_title.txt \
     --output \
     enter_high_score_nametable.py 
 
 python nametable_builder.py break \
     game_nametable.bak \
-    characters.txt \
+    characters_game.txt \
     --output \
     game_nametable.py 
 
 python nametable_builder.py break \
     game_type_menu_nametable.bak \
-    characters.txt \
+    characters_title.txt \
     --output \
     game_type_menu_nametable.py 
 
 python nametable_builder.py break \
     high_scores_nametable.bak \
-    characters.txt \
+    characters_title.txt \
     --output \
     high_scores_nametable.py --skip-attrs
 
 python nametable_builder.py break \
     legal_screen_nametable.bak \
-    characters.txt \
+    characters_title.txt \
     --output \
     legal_screen_nametable.py 
 
 python nametable_builder.py break \
     level_menu_nametable.bak \
-    characters.txt \
+    characters_title.txt \
     --output \
     level_menu_nametable.py 
 
 python nametable_builder.py break \
     title_screen_nametable.bak \
-    characters.txt \
+    characters_title.txt \
     --output \
     title_screen_nametable.py 
 
