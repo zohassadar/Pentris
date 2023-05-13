74f8dfef6b63297edd66760e7552afa3428ec8a6
Richard <zohassadar@gmail.com>
2023-05-12 20:35:57 -0700

    add demo

--- demo/recordDemo.lua
+++ demo/recordDemo.lua
@@ -29,14 +29,14 @@ end
 
 function registerListeners() 
   memory.register(0x00D0, done)     --adjacent to Demo Repeats
-  memory.registerexecute(0x9D47, handleButtons);  -- lda heldButton in @recording
-  memory.registerexecute(0x9D4E, handleRepeats);  -- lda repeats in @recording
+  memory.registerexecute(0x9B86, handleButtons);  -- lda heldButton in @recording right after ldx 00 and before sta (demoButtons,X)
+  memory.registerexecute(0x9B8D, handleRepeats);  -- lda repeats in @recording after jsr and before sta
 end
 
 function unregisterListeners()
   memory.register(0x00D0, nil)
-  memory.registerexecute(0x9D47, nil)
-  memory.registerexecute(0x9D4E, nil)
+  memory.registerexecute(0x9B86, nil)
+  memory.registerexecute(0x9B8D, nil)
 end
 
 function startDemoRecordingMode()
@@ -53,12 +53,12 @@ end
 
 function handleButtons() 
   bytes = bytes .. string.format("%02X ", memory.readbyte(0x00CE))
-  memory.setregister("pc", 0x9D49);   -- 1 byte after JSR demoButtonsTable in @recording
+  memory.setregister("pc", 0x9B88);   -- First occurence of sta (demoButtonsTable,X)
 end
 
 function handleRepeats() 
   bytes = bytes .. string.format("%02X ", memory.readbyte(0x00CF))
-  memory.setregister("pc", 0x9D50);    -- sta (demoButtonsTable,x) in @recording
+  memory.setregister("pc", 0x9B8F);    -- Second occurance
 end
 
 function done()
