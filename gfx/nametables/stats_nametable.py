19d5b318c9e9711212e06ceed283e300939bdf6c
zohassadar <zohassadar@gmail.com>
2024-10-16 22:07:40 +0000

    Centralize node rle & move rects to python

--- gfx/nametables/stats_nametable.py
+++ gfx/nametables/stats_nametable.py
@@ -96,21 +96,48 @@ starting_addresses = [(40, 0), (40, 32), (40, 64), (40, 96), (40, 128), (40, 160
  (42, 192), (42, 224), (43, 0), (43, 32), (43, 64), (43, 96), (43, 128), (43, 160), (43, 192), (43, 224)]  # fmt: skip
 
 
+rects = [
+    (1, 6, 4, 3, 16),  # U
+    (1, 9, 4, 3, 64),  # W
+    (1, 12, 4, 3, 112),  # V
+    (1, 15, 4, 3, 160),  # T
+    (1, 18, 4, 3, 208),  # X
+    (1, 22, 4, 1, 228),  # I
+    (9, 6, 4, 3, 20),  # J
+    (9, 9, 4, 3, 68),  # Y1
+    (9, 12, 4, 3, 116),  # N1
+    (9, 15, 4, 3, 164),  # F1
+    (9, 18, 4, 3, 124),  # S
+    (9, 21, 4, 3, 28),  # Q
+    (17, 6, 4, 3, 24),  # L
+    (17, 9, 4, 3, 72),  # Y2
+    (17, 12, 4, 3, 120),  # N2
+    (17, 15, 4, 3, 168),  # F2
+    (17, 18, 4, 3, 172),  # Z
+    (17, 21, 4, 3, 76),  # P
+    (1, 24, 2, 1, 0xD4),  # T4
+    (7, 24, 2, 1, 0xD6),  # J4
+    (13, 24, 2, 1, 0xD8),  # Z4
+    (19, 24, 2, 1, 0xDA),  # O4
+    (3, 26, 2, 1, 0xDC),  # S4
+    (9, 26, 2, 1, 0xDE),  # L4
+    (15, 26, 2, 1, 0xF4),  # I4
+]
+
 
 if __name__ == "__main__":
     try:
-        raw_output = file.parent / output.name.replace(".bin", ".raw")
         nametable_builder.build_nametable(
-            raw_output,
+            output,
             table,
             attributes,
             characters,
             original_sha1sum,
             lengths,
             starting_addresses,
+            rle_compress=True,
+            rects=rects,
         )
-        subprocess.run(["node", str(file.parent / "compress.js"), raw_output, output, "drawStats"])
-        raw_output.unlink()
     except Exception as exc:
         print(
             f"Unable to build nametable: {type(exc).__name__}: {exc!s}", file=sys.stderr
